94dbe334112d2a7dbe7a15f2bfc1d4a359381682
David <davidagamez04@gmail.com>
2024-09-27 17:13:11 -0500

    feat: catalog context seed created

---
+++ Services/Catalog/Catalog.infrastructure/Data/TypeContextSeed.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace Catalog.Infrastructure.Data {
+    public static class typecontextSeed {
+        public static void SeedData(IMongoCollection<ProductType> TypeCollection) {
+            bool checkTypes = TypeCollection.Find(b => true).Any();
+            string path = Path.Combine("Data", "SeedData", "types.json");
+            if (!checkTypes) {
+                var typesData = File.ReadAllText(path);
+                var types = JsonSerializer.Deserialize<List<ProductBrand>>(typesData);
+                if (types != null) {
+                    foreach (var type in types) {
+                        TypeCollection.InsertOneAsync(type);
+                    }
+                }
+            }
+        }
+    }
+}
